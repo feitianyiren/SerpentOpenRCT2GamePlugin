d67ca1cf7d07da981259ca981b873019fdcc64fe
Nicholas Brochu <info@nicholasbrochu.com>
2017-09-15 15:00:59 -0400

    update

--- files/api/api.py
+++ files/api/api.py
@@ -1,5 +1,9 @@
 from lib.game_api import GameAPI
 
+from lib.game_frame import GameFrame
+
+import time
+
 
 class OpenRCT2API(GameAPI):
 
@@ -7,39 +11,81 @@ class OpenRCT2API(GameAPI):
         super().__init__(game=game)
 
     class MainMenu:
+        scenario_tab_name_mapping = {
+            "RollerCoaster Tycoon": "MAIN_MENU_SCENARIO_TAB_RCT",
+            "Corkscrew Follies": "MAIN_MENU_SCENARIO_TAB_RCT_CF",
+            "Loopy Landscapes": "MAIN_MENU_SCENARIO_TAB_RCT_LL",
+            "RollerCoaster Tycoon 2": "MAIN_MENU_SCENARIO_TAB_RCT2",
+            "Wacky Worlds": "MAIN_MENU_SCENARIO_TAB_RCT2_WW",
+            "Time Twister": "MAIN_MENU_SCENARIO_TAB_RCT2_TT",
+            "Real Parks": "MAIN_MENU_SCENARIO_TAB_RCT_RP",
+            "Other Parks": "MAIN_MENU_SCENARIO_TAB_RCT_OP"
+        }
+
+        @classmethod
+        def open_scenario(cls, game_frame, scenario_tab_name="RollerCoaster Tycoon", scenario_name="Forest Frontiers", lastest_frame_func=None):
+            cls.click_new_game()
+            time.sleep(0.5)
+            cls.click_scenario_tab(scenario_tab_name)
+            time.sleep(0.5)
+
+            latest_game_frame = lastest_frame_func()
+
+            offset_x = game_frame.offset_x
+            offset_y = game_frame.offset_y
+
+            game_frame = GameFrame(latest_game_frame.frame[
+                offset_y:offset_y + game_frame.frame.shape[0],
+                offset_x:offset_x + game_frame.frame.shape[1],
+            ], offset_x=offset_x, offset_y=offset_y)
+
+            cls.click_scenario(scenario_name, game_frame)
+
         @classmethod
         def click_new_game(cls):
             OpenRCT2API.instance.input_controller.click_screen_region(
-                screen_region="MAIN_MENU_NEW_GAME",
-                game=OpenRCT2API.instance.game
+                screen_region="MAIN_MENU_NEW_GAME"
             )
 
         @classmethod
         def click_load_game(cls):
             OpenRCT2API.instance.input_controller.click_screen_region(
-                screen_region="MAIN_MENU_LOAD_GAME",
-                game=OpenRCT2API.instance.game
+                screen_region="MAIN_MENU_LOAD_GAME"
             )
 
         @classmethod
         def click_multiplayer(cls):
             OpenRCT2API.instance.input_controller.click_screen_region(
-                screen_region="MAIN_MENU_MULTIPLAYER",
-                game=OpenRCT2API.instance.game
+                screen_region="MAIN_MENU_MULTIPLAYER"
             )
 
         @classmethod
         def click_game_tools(cls):
             OpenRCT2API.instance.input_controller.click_screen_region(
-                screen_region="MAIN_MENU_GAME_TOOLS",
-                game=OpenRCT2API.instance.game
+                screen_region="MAIN_MENU_GAME_TOOLS"
             )
 
         @classmethod
         def click_quit(cls):
             OpenRCT2API.instance.input_controller.click_screen_region(
-                screen_region="MAIN_MENU_QUIT",
-                game=OpenRCT2API.instance.game
+                screen_region="MAIN_MENU_QUIT"
+            )
+
+        @classmethod
+        def click_scenario_tab(cls, scenario_tab_name):
+            screen_region_name = cls.scenario_tab_name_mapping.get(scenario_tab_name)
+
+            OpenRCT2API.instance.input_controller.click_screen_region(
+                screen_region=screen_region_name
+            )
+
+        @classmethod
+        def click_scenario(cls, scenario_name, game_frame):
+            clicked = OpenRCT2API.instance.input_controller.click_string(
+                scenario_name,
+                game_frame,
+                fuzziness=2,
+                ocr_preset=OpenRCT2API.instance.game.ocr_presets.get("SCENARIO_TEXT")
             )
 
     class Game:
--- files/serpent_OpenRCT2_game.py
+++ files/serpent_OpenRCT2_game.py
@@ -27,9 +27,36 @@ class SerpentOpenRCT2Game(Game, metaclass=Singleton):
             "MAIN_MENU_MULTIPLAYER": (626, 511, 708, 592),
             "MAIN_MENU_GAME_TOOLS": (626, 593, 708, 674),
             "MAIN_MENU_QUIT": (704, 983, 768, 1023),
+            "MAIN_MENU_SCENARIO_TAB_RCT": (234, 148, 267, 239),
+            "MAIN_MENU_SCENARIO_TAB_RCT_CF": (234, 239, 267, 330),
+            "MAIN_MENU_SCENARIO_TAB_RCT_LL": (234, 330, 267, 421),
+            "MAIN_MENU_SCENARIO_TAB_RCT2": (234, 421, 267, 512),
+            "MAIN_MENU_SCENARIO_TAB_RCT2_WW": (234, 512, 267, 603),
+            "MAIN_MENU_SCENARIO_TAB_RCT2_TT": (234, 603, 267, 694),
+            "MAIN_MENU_SCENARIO_TAB_RCT_RP": (234, 694, 267, 785),
+            "MAIN_MENU_SCENARIO_TAB_RCT_OP": (234, 785, 267, 876),
             "GAME_PAUSE_BUTTON": (0, 0, 27, 30),
             "GAME_SPEED_BUTTON": (0, 30, 27, 60),
             "GAME_FLOPPY_BUTTON": (0, 60, 27, 90)
         }
 
         return regions
+
+    @property
+    def ocr_presets(self):
+        presets = {
+            "SCENARIO_TEXT": {
+                "extract": {
+                    "gradient_size": 3,
+                    "closing_size": 10
+                },
+                "perform": {
+                    "scale": 16,
+                    "order": 3,
+                    "horizontal_closing": 1,
+                    "vertical_closing": 1
+                }
+            }
+        }
+
+        return presets
